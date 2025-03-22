bf681077e67e4f385f4f329592244cc991195eab
Michael Maclean <michael@mgdm.net>
2025-03-22 18:47:28 +0000

    Add comments

--- flake.nix
+++ flake.nix
@@ -20,6 +20,9 @@
           }; in
             pkgs.runCommand "ja4-nginx-module" { } ''
               cp -a ${src'} $out
+
+              # The nginx compilation with -Werror fails without this as the variable
+              # is not known to be initialised in the if/else chain
               substituteInPlace $out/src/ngx_http_ssl_ja4_module.c \
                 --replace 'double propagation_delay_factor;' 'double propagation_delay_factor = 1.0;'
           '';
@@ -28,6 +31,8 @@
         openssl-ja4 = pkgs.openssl.overrideAttrs (drv: {
           patches = (drv.patches or [ ])
             ++ [ "${nginx-ja4-module.src}/patches/openssl.patch" ];
+           # On Linux, we need to run `make update` after configuring so that the new
+           # function added by the patch isn't stripped out by the linker
            postConfigure = ''
             make update
           '';
